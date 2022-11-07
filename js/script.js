ba70c18b39dcf82669f7a3729589c1f68f34d4a4
danieferra <danieferra@gmail.com>
2022-11-07 10:41:07 +0000

    acrescentar sequencia para menu

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ function changeMenu(place) {
     })
     document.getElementById(place).style.display="block";
   }
+
+function avancarMenu(){
+    modals = document.getElementsByClassName("modal-body");
+    Array.from(modals).forEach(function(elem) {
+        if (elem.style.display!="none"){
+            aberto = parseInt(elem.dataset.target)+1;
+            elem.style.display="none";
+        }
+
+    })
+    modal="seq-menu"+aberto.toString();
+    document.getElementById(modal).style.display="block";
+}
