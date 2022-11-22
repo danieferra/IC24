d517bc1277fda8ea0f3713567b724c5929c6a37a
danieferra <danieferra@gmail.com>
2022-11-22 14:00:47 +0000

    imagens e menu

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+var pratos = [];
+pratos.push({
+    nome: "Bitoque",
+    descricao: "O clássico bife com ovo a cavalo, com várias opções de acompanhamentos que pode personalizar, com direito a bebida e sobremesa.",
+    infoNut: "<div class='col-6 infoNut'> <div class='font-weight-bolder'>Informação Nutricional</div> <p>Sem Glúten</p> <table> <tbody><tr> <td><b>Energia</b></td> <td><b>2109 kj</b></td> <td><b>25%</b></td> </tr> <tr> <td></td> <td>504 kcal</td> <td></td> </tr> <tr> <td><b>Lípidos</b></td> <td><b>30,02g</b></td> <td><b>43%</b></td> </tr> <tr> <td><b>Lípidos Saturados</b></td> <td><b>11,754g</b></td> <td><b>59%</b></td> </tr> <tr> <td>Lípidos Monoinsaturados</td> <td>12,559g</td> <td></td> </tr> <tr> <td>Lípidos Poliinsaturados</td> <td>1,118g</td> <td></td> </tr> <tr> <td><b>Carboidratos</b></td> <td><b>0g</b></td> <td><b>0%</b></td> </tr> </tbody></table> </div>",
+    preco: "14,50"
+  });
+  
+  pratos.push({
+    nome: "Bacalhau à Brás",
+    descricao: "Aquele bacalhau, lasca que é um mimo! O que é bom é português!",
+    infoNut: "<div class='col-6 infoNut'> <div class='font-weight-bolder'>Informação Nutricional</div> <p>Sem Glúten</p> <table> <tbody><tr> <td><b>Energia</b></td> <td><b>2109 kj</b></td> <td><b>25%</b></td> </tr> <tr> <td></td> <td>504 kcal</td> <td></td> </tr> <tr> <td><b>Lípidos</b></td> <td><b>30,02g</b></td> <td><b>43%</b></td> </tr> <tr> <td><b>Lípidos Saturados</b></td> <td><b>11,754g</b></td> <td><b>59%</b></td> </tr> <tr> <td>Lípidos Monoinsaturados</td> <td>12,559g</td> <td></td> </tr> <tr> <td>Lípidos Poliinsaturados</td> <td>1,118g</td> <td></td> </tr> <tr> <td><b>Carboidratos</b></td> <td><b>0g</b></td> <td><b>0%</b></td> </tr> </tbody></table> </div>",
+    preco: "14,50"
+});
