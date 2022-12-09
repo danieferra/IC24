86621f6264e6c9a0b6704eea3af922119657d84e
danieferra <danieferra@gmail.com>
2022-12-09 18:04:14 +0000

    acabou malta

--- js/reserva.js
+++ js/reserva.js
@@ -12,13 +12,18 @@ function checkForm() {
     var horaReserva = document.getElementById("horaReserva").value;
     if (nomeReserva !="" && /^([0-9]{9})$/.test(numeroReserva) && dataReserva != "" && numPessoas != "" && horaReserva != "") {
         document.getElementById("reservaBtn").disabled = false;
+        document.getElementById('numeroReservaId').classList.remove('highlight');
+        document.getElementById('numeroNotificacao').style.display = "none";
     }
-    if (!(/^([0-9]{9})$/.test(numeroReserva))) {
+    else if (!(/^([0-9]{9})$/.test(numeroReserva))) {
         document.getElementById('numeroReservaId').classList.add('highlight');
         document.getElementById('numeroNotificacao').style.display = "flex";
+        document.getElementById("reservaBtn").disabled = true;
     }
     else {
         document.getElementById("reservaBtn").disabled = true;
+        document.getElementById('numeroNotificacao').style.display = "none";
+        document.getElementById('numeroReservaId').classList.remove('highlight');
     }
 }
 
@@ -43,8 +48,8 @@ function reserva() {
     var numPessoas = parseInt(document.getElementById("numPessoas").innerHTML);
     var horaReserva = document.getElementById("horaReserva").value;
     
-    document.getElementById("nomeReserva").innerHTML = nomeReserva;
-    document.getElementById("numeroReserva").innerHTML = numeroReserva;
+    document.getElementById("nomeReservaId2").innerHTML = nomeReserva;
+    document.getElementById("numeroReservaId2").innerHTML = numeroReserva;
     document.getElementById("dataReserva").innerHTML = dataReserva;
     document.getElementById("numPessoasShow").innerHTML = numPessoas;
     document.getElementById("horaReservaShow").innerHTML = horaReserva;
